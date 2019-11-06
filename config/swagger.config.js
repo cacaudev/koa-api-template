72d51ca80d429c9ace4ec0f3a65473b47951fa96
cacaudev <cacauabreu2013@gmail.com>
2019-11-06 17:47:01 -0200

    [feat] (Docs): Add swagger config to be used on jsdoc

---
+++ config/swagger.config.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const swaggerJsdoc = require('swagger-jsdoc');
+const appInfo = require('../package.json');
+
+const info = {
+  title: appInfo.name,
+  description: appInfo.description,
+  version: appInfo.version,
+  contact: appInfo.author,
+  termsOfService: ''
+};
+const servers = [];
+
+// Swagger definitions
+const definition = {
+  openapi: '3.0.0',
+  info,
+  servers
+};
+
+// Options for the swagger specification
+const options = {
+  definition,
+  // Path to the API specs
+  apis: [
+    __dirname + '/../src/controllers/*.js',
+  ]
+};
+const swaggerSpec = swaggerJsdoc(options);
+
+module.exports = swaggerSpec;
