2dea601da729c66c2dfb31a4f0f9584589e7eb0f
cacaudev <cacauabreu2013@gmail.com>
2019-11-29 09:50:03 -0200

    [feat] (Tests): Separate users mock example on separate folder

--- db/models/user.mock.js
+++ db/models/user.mock.js
@@ -1,30 +1,12 @@
 'use strict';
 
-module.exports = (sequelizeMock) => {
-  const User = sequelizeMock.define('User',
-    {
-      id: 'JqBj5Rwe0Z8H4U05wU9yxZE9YeT2X5Bhy2po',
-      username: 'testdev',
-      password: '$2a$10$pGA9QcTBlIIibT9DcTIHQ.IftaPVQ9GAcXyJURhQYAP97vdFRW1x.',
-      name: 'test',
-      surname: 'dev',
-      age: 26,
-      timezone: 'America/Sao_Paulo',
-      updatedAt: '2019-11-27T19:06:26.373Z',
-      createdAt: '2019-11-27T19:06:26.373Z'
-    },
-    {
-      id: 'fQBXIMXF9AkffJhprKEFob0ONN5pUQTeeoka',
-      username: 'thisisatest',
-      password: '$2a$12$MOjaG6HbCkTB966I4Qkr7.TJlHdN.RDHPnkxJSkLyc0uymsRhXnqi',
-      name: 'Cacau',
-      surname: 'Developer',
-      age: 26,
-      timezone: 'America/Sao_Paulo',
-      updatedAt: '2019-11-28T19:06:26.373Z',
-      createdAt: '2019-11-28T19:06:26.373Z'
-    }
-  );
+var mock_users = require('../mock_examples/users.json');
 
+module.exports = (sequelizeMock) => {
+  let User = sequelizeMock.define('User');
+  User.$queueResult([
+    User.build(mock_users[0]),
+    User.build(mock_users[1])
+  ]);
   return User;
 };
